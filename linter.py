66f7d108386709d5409a0f87007b50ac91b92a16
alihsaas <40891677+alihsaas@users.noreply.github.com>
2020-10-07 22:32:03 +0300

    Update linter.py
    update to read from stdin

--- linter.py
+++ linter.py
@@ -2,13 +2,12 @@ from SublimeLinter.lint import Linter
 
 
 class Selene(Linter):
-    cmd = ('selene', '${file}', '--display-style=quiet')
+    cmd = ('selene', '-', '${file}', '--display-style=quiet')
     regex = (
         r'.+:(?P<line>\d+):(?P<col>\d+): '
         r'(?:warning\[(?P<warning>.+)|error\[(?P<error>.+))\]: '
         r'(?P<message>.+)'
     )
-    tempfile_suffix = '-'
     defaults = {
         'selector': 'source.lua'
     }
